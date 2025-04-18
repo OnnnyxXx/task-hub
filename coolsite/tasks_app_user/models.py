c1ffca33e4ef0e17ccc9d3c5064cc4562bb2dda5
OnnnyxXx <oooivanlau2@gmail.com>
2025-04-18 09:31:19 +0300

    upd(create): visually updated upd(model): added by default

--- coolsite/tasks_app_user/models.py
+++ coolsite/tasks_app_user/models.py
@@ -13,8 +13,8 @@ class Category(models.Model):
 class Articles(models.Model):
     title = models.CharField("Название Задания", max_length=35)
     anons = models.CharField("О задании", max_length=25)
-    phope = models.CharField('Телефон для связи с вами', max_length=11)
-    prise = models.CharField('Цена', max_length=15)
+    phope = models.CharField("Телефон для связи с вами", max_length=11, default='NO')
+    prise = models.CharField("Цена", max_length=15)
     full_text = models.TextField("Задания")
     data = models.DateTimeField(auto_now_add=True)
     author = models.ForeignKey(User, on_delete=models.CASCADE, null=True)  # Внешний ключ к пользователю
@@ -32,4 +32,3 @@ class Articles(models.Model):
 
     def get_absolute_url(self):
         return f'/tasks/{self.id}'
-
